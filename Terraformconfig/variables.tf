9260903e3dd032e92d9d544e9fbf1734fac10be1
Mathew Danish <mathew.danish@genesys.com>
2022-10-28 21:31:01 -0600

    last commit i promise

--- Terraformconfig/variables.tf
+++ Terraformconfig/variables.tf
@@ -1,13 +1,13 @@
 variable "oauthclient_id" {
-  default = "5da2b7be-3617-4145-b1a1-1e5a2ca8d8a2"
+  default = "insertauthid"
 }
 variable "oauthclient_secret" {
-  default = "G9lDG8HY0yiw4g9LDcYmjBfpDT2wLW54SmUfW0nb5yc"
+  default = "insersecret"
 }
 
 variable "aws_region" {
   default = "us-west-2"
 }
 variable "password" {
-  default = "Indyworkshop1!"
+  default = "insertpaswword"
 }
